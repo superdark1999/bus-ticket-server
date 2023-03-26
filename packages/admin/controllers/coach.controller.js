6192df53d6414401324f66a440669c36efffcfa1
Nguyen Phi Long <philongit1999@gmail.com>
2023-03-26 14:07:51 +0700

    add catch async

--- packages/admin/controllers/coach.controller.js
+++ packages/admin/controllers/coach.controller.js
@@ -1,15 +1,16 @@
 import pick from "shared/lib/utils/pick";
+import catchAsync from "shared/lib/utils/catchAsync";
 import coachService from "../services/coach.service";
 
-const list = async (req, res) => {
+const list = catchAsync(async (req, res) => {
   const filter = pick(req.query, ["startDate"]);
   const options = pick(req.query, ["sortBy", "limit", "page"]);
 
   const result = await coachService.getCoaches(filter, options);
   return res.status(200).json({ data: result });
-};
+});
 
-const getById = () => {};
+const getById = catchAsync(() => {});
 
 export const coachController = {
   list,
