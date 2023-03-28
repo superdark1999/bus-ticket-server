81606d722a4d873ad1509c97eaa0f678b08df7a2
Trần Cường <trancuongitus@gmail.com>
2023-03-29 00:45:53 +0700

    Register user and login API.

--- packages/auth/controllers/auth.controller.js
+++ packages/auth/controllers/auth.controller.js
@@ -1,15 +1,41 @@
 import authService from "../services/auth.service"
 
 const create = register(async (req, res) => {
+    try {
+        const param = {
+            enabled: true,
+            credentials: [
+                {
+                    type: 'password',
+                    value: req.body.password,
+                    temporary: false
+                }
+            ],
+            username: req.body.username,
+            email: req.body.email,
+            emailVerified: true,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        }
 
-    const result = await authService.register(/*Add param */);
-    return res.status(200).json({ data: result });
+        const result = await authService.register(param);
+        return res.status(200).json({ data: result });
+    } catch (error) {
+        return res.status(500).json({ message: 'Interal Server Error' });
+    }
 });
 
 const userLogin = login(async (req, res) => {
-
-    const result = await authService.login(/*Add param */);
-    return res.status(200).json({ data: result });
+    try {
+        const param = {
+            username: req.body.username,
+            password: req.body.password,
+        }
+        const result = await authService.login(param);
+        return res.status(200).json({ data: result });
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 });
 
 const userLogout = logout(async (req, res) => {
