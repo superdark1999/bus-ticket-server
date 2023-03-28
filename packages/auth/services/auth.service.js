aa1078411b1ce1bc5e64e02e57db18f68032d6ec
Trần Cường <trancuongitus@gmail.com>
2023-03-29 00:45:53 +0700

    Register user and login API.

--- packages/auth/services/auth.service.js
+++ packages/auth/services/auth.service.js
@@ -1,9 +1,53 @@
-const register = async (req, res) => {
-    return '';
+import axios from "axios";
+import qs from "node:querystring";
+import jwt from "jsonwebtoken"
+
+import session from "express-session"
+import keycloak from "keycloak-connect"
+
+const MEMORY_STORE = new session.MemoryStore();
+const KEY_CLOAK = new keycloak({ store: MEMORY_STORE });
+
+const register = async (param) => {
+    try {
+        const tokenResponse = await axios.post('http://localhost:8080/auth/realms/bus-ticket-auth-realm/protocol/openid-connect/token', qs.stringify({
+            grant_type: 'password',
+            username: 'admin',
+            password: 'admin',
+            client_id: 'bus-ticket-server',
+            client_secret: '4UCFdrgGkqIS2mP3MevAFRLrs9j8jGOm'
+        }), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        const accessToken = tokenResponse.data.access_token;
+
+        const createUserResponse = await axios.post('http://localhost:8080/auth/admin/realms/bus-ticket-auth-realm/users', param, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+
+        return createUserResponse.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
 
-const login = async (req, res) => {
-    return '';
+const login = async (param) => {
+    const grant = await KEY_CLOAK.grantManager.obtainDirectly(param.username, param.password);
+    if (null === grant) {
+        return null;
+    }
+
+    const userDecode = jwt.decode(grant.id_token);
+    const result = {
+        grant: grant,
+        user: userDecode
+    }
+    return result;
 }
 
 const logout = async (req, res) => {
