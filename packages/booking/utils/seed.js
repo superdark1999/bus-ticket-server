716c3a7b85785d4eb72769419c4d94912812d1eb
Nguyen Van Loi <nguyenvanloi10101@gmail.com>
2023-04-03 16:45:58 +0700

    drop old collections and seed collections

--- packages/booking/utils/seed.js
+++ packages/booking/utils/seed.js
@@ -1,7 +1,19 @@
+import { Model } from "mongoose";
 import models from "../models";
 
 export default async function seed() {
-  // seed data for triproute
+  // trip_route
+  // delete old collection in database
+  const drop = await models.trip_route.collection.drop();
+  if(drop){
+    console.log('old trip route collection was deleted');
+  }
+  else{
+    console.log('error drop old trip route collection')
+    return;
+  }
+
+  // seed data for trip_route
   const tripRoutes = [
     {
       origin: "Sài Gòn",
@@ -37,6 +49,6 @@ export default async function seed() {
 
   await models.trip_route
     .insertMany(tripRoutes)
-    .then(() => console.log("trip routes was inserted"))
+    .then(() => console.log("trip route collections was inserted"))
     .catch((err) => console.error(err));
 }
