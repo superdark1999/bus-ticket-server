00cd80e38cd67a049a2b7e611cdcba3734c5c74e
Tran Bao Nguyen <nguyen.tran@devsamurai.com>
2023-04-05 04:10:36 +0700

    feat: BUS-44: add gRPG server

---
+++ packages/coach/services/coach.service.js
@@ -0,0 +1,85 @@
+import { Coach } from "../models";
+
+export function createCoach(call, callback) {
+  const coachData = call.request;
+
+  const coach = new Coach(coachData);
+
+  coach.save((err, doc) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    callback(null, doc.toObject());
+  });
+}
+
+export function getCoachList(call, callback) {
+  const page = call.request.page || 1;
+  const limit = call.request.limit || 10;
+
+  const options = {
+    page,
+    limit,
+  };
+
+  Coach.paginate({}, options, (err, result) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    const response = {
+      coaches: result.docs.map((doc) => doc.toObject()),
+      page: result.page,
+      limit: result.limit,
+      total: result.totalDocs,
+      totalPages: result.totalPages,
+    };
+
+    callback(null, response);
+  });
+}
+
+export function getCoach(call, callback) {
+  const { id } = call.request;
+
+  Coach.findById(id, (err, doc) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (!doc) {
+      callback(
+        { code: grpc.status.NOT_FOUND, message: "Coach not found" },
+        null
+      );
+      return;
+    }
+
+    callback(null, doc.toObject());
+  });
+}
+
+export function updateCoach(call, callback) {
+  const { id, ...updateData } = call.request;
+
+  Coach.findByIdAndUpdate(id, updateData, { new: true }, (err, doc) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (!doc) {
+      callback(
+        { code: grpc.status.NOT_FOUND, message: "Coach not found" },
+        null
+      );
+      return;
+    }
+
+    callback(null, doc.toObject());
+  });
+}
