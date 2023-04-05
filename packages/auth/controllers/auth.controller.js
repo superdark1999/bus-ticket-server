c8d324e27e1e40bfcff049f0990040334407de6f
Trần Cường <trancuongitus@gmail.com>
2023-04-05 23:12:39 +0700

    Update authentication service

--- packages/auth/controllers/auth.controller.js
+++ packages/auth/controllers/auth.controller.js
@@ -22,9 +22,18 @@ exports.register = async (req, res) => {
 
 
         const result = await authService.register(param);
-        return res.status(200).json({ data: result });
+
+        return res.status(result.status).json({
+            status: result.status,
+            statusText: result.statusText,
+            data: result.data,
+        });
     } catch (error) {
-        return res.status(500).json({ message: 'Interal Server Error' });
+        return res.status(error.response.status).json({
+            status: error.response.status,
+            statusText: error.response.statusText,
+            data: error.response.data,
+        });
     }
 }
 
@@ -35,9 +44,17 @@ exports.login = async (req, res) => {
             password: req.body.password,
         }
         const result = await authService.login(param);
-        return res.status(200).json({ data: result });
+
+        return res.status(200).json({
+            status: 200,
+            statusText: 'Success',
+            data: result,
+        });
     } catch (error) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({
+            status: 401,
+            statusText: 'Unauthorized',
+        });
     }
 };
 
@@ -63,7 +80,32 @@ exports.changePasswordPassive = async (req, res) => {
  * @description Change Password when user using change password function
  */
 exports.changePasswordActive = async (req, res) => {
+    try {
+        const param = {
+            id_token: req.body.id_token,
+            old_password: req.body.old_password,
+            new_password: req.body.new_password,
+        }
 
-    const result = await authService.changePasswordActive(/*Add param */);
-    return res.status(200).json({ data: result });
+        const result = await authService.changePasswordActive(param);
+
+        return res.status(result.status).json({
+            status: result.status,
+            statusText: result.statusText,
+            data: result.data,
+        })
+
+    } catch (error) {
+        console.log(error);
+        if (error.response.status == 401) {
+            if ('invalid_grant' === error.response.data.error) {
+                return res.status(error.response.status).json({
+                    status: error.response.status,
+                    statusText: 'Bad Request',
+                    data: error.response.data,
+                })
+            }
+        }
+
+    }
 };
