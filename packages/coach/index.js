5ea5e6d467bf42dc1f1a3183c1a610838514533a
Nguyen Phi Long <philongit1999@gmail.com>
2023-04-16 17:19:40 +0700

    update

--- packages/coach/index.js
+++ packages/coach/index.js
@@ -1,17 +1,18 @@
-import grpc from "grpc";
 import protoLoader from "@grpc/proto-loader";
+import grpc from "grpc";
+import connectMongoDB from "shared/lib/db/mongodb";
 import {
   createCoach,
+  deleteCoach,
   getCoach,
   getCoachList,
   updateCoach,
-  deleteCoach,
 } from "./services/coach.service.js";
+
 const packageDefinition = protoLoader.loadSync("./coach.proto");
 const coachProto = grpc.loadPackageDefinition(packageDefinition);
-import connectMongoDB from "shared/lib/db/mongodb";
-// import { connectConsumer } from "./kafka/kafka";
 const server = new grpc.Server();
+
 const bootstrap = () => {
   connectMongoDB();
 
@@ -23,7 +24,7 @@ const bootstrap = () => {
     deleteCoach,
   });
 
-  server.bindAsync(
+  server.bind(
     "localhost:50051",
     grpc.ServerCredentials.createInsecure(),
     () => {
@@ -31,8 +32,6 @@ const bootstrap = () => {
       console.log("Server running at grpc://localhost:50051");
     }
   );
-
-  // connectConsumer();
 };
 
 bootstrap();
