5777ac38c8c60b031153e3df96d94f3e629e129b
anhminh0401 <nguyenanhminh04012001@gmail.com>
2023-06-10 14:55:55 +0700

    get list trip route

--- packages/trip-routes/controllers/tripRoute.controller.js
+++ packages/trip-routes/controllers/tripRoute.controller.js
@@ -2,9 +2,9 @@ import tripRouteService from "../services/tripRoute.service";
 
 const createNewTripRoute = async (req, res) => {
   try{
-    const {departureTime, arrivalTime, trip_id, coach_id, capacity} = req.body;
-    const newTripRoute = await tripRouteService.createNewTripRoute(departureTime, arrivalTime, trip_id, coach_id, capacity);
-    res.status(200).json({newCoach: newTripRoute});
+    const {departureTime, arrivalTime, trip_id, coach_id, bookedSeat} = req.body;
+    const newTripRoute = await tripRouteService.createNewTripRoute(departureTime, arrivalTime, trip_id, coach_id, bookedSeat);
+    res.status(200).json({newTripRoute: newTripRoute});
   }
   catch(error){
     console.log(error);
@@ -15,7 +15,7 @@ const createNewTripRoute = async (req, res) => {
 const getTripRouteList = async (req, res) => {
   try{
     const tripRouteList = await tripRouteService.getTripRoutes();
-    res.status(200).json({coachList: tripRouteList})
+    res.status(200).json({tripRouteList: tripRouteList})
   }
   catch(error){
     console.log(error);
