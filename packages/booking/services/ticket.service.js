3213cc3099a3c6891c2d80a54c714518d8f78e99
Nguyen Van Loi <nguyenvanloi10101@gmail.com>
2023-06-14 12:28:11 +0700

    fix baseur

--- packages/booking/services/ticket.service.js
+++ packages/booking/services/ticket.service.js
@@ -70,7 +70,7 @@ const ticketService = {
       };
 
       await axios.put(
-        `${process.env.TRIP_ROUTE_SERVICE_URL}/${tripRoute_id}`,
+        `${process.env.TRIP_ROUTE_SERVICE_URL}/trip-route/trip-route/${tripRoute_id}`,
         data
       );
 
